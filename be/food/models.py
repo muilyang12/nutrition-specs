894c0ee192895c348491fc0b443710e70103acc1
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-15 19:21:13 +0900

    food 앱 내 model 추가.

--- be/food/models.py
+++ be/food/models.py
@@ -2,3 +2,31 @@ from django.db import models
 
 
 # Create your models here.
+class FoodCategory(models.Model):
+    category_name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    food_category = models.ForeignKey(
+        FoodCategory, on_delete=models.SET_NULL, null=True
+    )
+    brand_name = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.brand_name} - {self.product_name} kcal"
+
+
+class Nutrition(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    serving_size = models.FloatField()
+    calory = models.FloatField()
+    carbohydrate = models.FloatField()
+    protein = models.FloatField()
+    fat = models.FloatField()
+
+    def __str__(self):
+        return f"{self.product.product_name} - {self.calory} kcal"
