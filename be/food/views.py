62828bb470330889a45990641e502fa4712961d4
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-19 18:50:08 +0900

    product와 nutrition에 대한 api query parms에 대한 처리 추가.

--- be/food/views.py
+++ be/food/views.py
@@ -1,5 +1,6 @@
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 from . import models, serializers
 
 
@@ -12,7 +13,29 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
 
+    def list(self, request, *args, **kwargs):
+        food_category_id = request.query_params.get("food-category")
+
+        if not food_category_id:
+            raise ValidationError(code=status.HTTP_400_BAD_REQUEST)
+
+        result = self.queryset.filter(food_category_id=food_category_id)
+        serializer = self.get_serializer(result, many=True)
+
+        return Response(serializer.data)
+
 
 class NutritionViewSet(viewsets.ModelViewSet):
     queryset = models.Nutrition.objects.all()
     serializer_class = serializers.NutritionSerializer
+
+    def list(self, request, *args, **kwargs):
+        product_ids = request.query_params.getlist("product")
+
+        if not product_ids:
+            raise ValidationError(code=status.HTTP_400_BAD_REQUEST)
+
+        result = self.queryset.filter(product__id__in=product_ids)
+        serializer = self.get_serializer(result, many=True)
+
+        return Response(serializer.data)
