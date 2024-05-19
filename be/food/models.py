7fdbcb6f9eea322151eb1c0a3eb7e16a9f45f112
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-19 10:51:00 +0900

    food category 모델에 unique 옵션 추가.

--- be/food/models.py
+++ be/food/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class FoodCategory(models.Model):
-    category_name = models.CharField(max_length=100, db_index=True)
+    category_name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.category_name
