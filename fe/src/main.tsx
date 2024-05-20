0adba6ad66b7ccf7e0cba5f0d18a841edc8c62fa
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-20 09:15:04 +0900

    fe 배포를 위한 Dockerfile 완성.

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/home";
@@ -10,8 +9,4 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
