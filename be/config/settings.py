550b101e644e9d8d9bcd9e7b3cd975c1cee5cdb6
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-21 10:05:33 +0900

    be 배포를 위한 Dockerfile 완성.

--- be/config/settings.py
+++ be/config/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -23,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-k42pd-)0q%%8&crxov&=hd@1j@$@bvzj$alb2mphs98u)q&-se"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+isProd = os.getenv('IS_PROD', 'False') == 'True'
+print('Running production mode' if isProd else 'Running development mode')
 
+DEBUG = False if isProd else True
+
+ALLOWED_HOSTS = ["localhost"] if isProd else []
 
 # Application definition
 
