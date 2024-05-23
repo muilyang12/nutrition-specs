d7b410e93eb9a414dd69d3c8446edd477076f675
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-23 10:17:45 +0900

    product list api에서 category-key를 query로도 응답할 수 있도록 추가.

--- be/food/views.py
+++ be/food/views.py
@@ -14,15 +14,27 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.ProductSerializer
 
     def list(self, request, *args, **kwargs):
-        food_category_id = request.query_params.get("food-category")
+        category_id = request.query_params.get("food-category")
+        category_key = request.query_params.get("category-key")
 
-        if not food_category_id:
+        if not category_id and not category_key:
             raise ValidationError(code=status.HTTP_400_BAD_REQUEST)
 
-        result = self.queryset.filter(food_category_id=food_category_id)
-        serializer = self.get_serializer(result, many=True)
+        data = None
 
-        return Response(serializer.data)
+        if category_id:
+            result = self.queryset.filter(food_category_id=category_id)
+            serializer = self.get_serializer(result, many=True)
+            data = serializer.data
+
+        elif category_key:
+            result = self.queryset.select_related("food_category").filter(
+                food_category__category_key=category_key
+            )
+            serializer = self.get_serializer(result, many=True)
+            data = serializer.data
+
+        return Response(data)
 
 
 class NutritionViewSet(viewsets.ModelViewSet):
