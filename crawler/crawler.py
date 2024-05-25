aa5eb33282a718f7b8e0482e828d9ac13e1b9e47
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-25 12:12:09 +0900

    details.json 파일 저장 로직 별도 함수로 분리.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,37 +1,9 @@
-import os
-import json
-
 from modules import (
     get_products,
-    coupang_get_with_headers,
+    save_product_details,
 )
 
 products = get_products(searchString="두유")
 
 for product in products:
-    name, productId, itemId, vendorItemId = (
-        product["name"],
-        product["productId"],
-        product["itemId"],
-        product["vendorItemId"],
-    )
-
-    response = coupang_get_with_headers(
-        f"https://www.coupang.com/vp/products/{productId}/items/{itemId}/vendoritems/{vendorItemId}"
-    )
-    data = response.json()
-
-    if "details" in data:
-        details = data["details"]
-
-        save_dir = os.path.join(".", "data")
-        product_name_dir = os.path.join(save_dir, name)
-        os.makedirs(product_name_dir, exist_ok=True)
-
-        file_path = os.path.join(product_name_dir, "details.json")
-        with open(file_path, "w", encoding="utf-8") as file:
-            details_dict = {"details": details}
-            json.dump(details_dict, file, ensure_ascii=False, indent=4)
-
-    else:
-        print("details 속성이 응답에 없습니다.")
+    save_product_details(product=product)
--- crawler/modules/__init__.py
+++ crawler/modules/__init__.py
@@ -1,4 +1,4 @@
 from .extract_headers import extract_headers
-from .start_driver import start_driver
 from .get_products import get_products
 from .coupang_get_with_headers import coupang_get_with_headers
+from .save_product_details import save_product_details
