9ce48bfceb1a6c30137ee248d8cdce9d7fa47cd0
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-05-31 11:39:40 +0900

    누락된 image_path 오픈 로직 추가.

--- crawler/modules/image/get_preprocessed_image.py
+++ crawler/modules/image/get_preprocessed_image.py
@@ -2,9 +2,10 @@ from PIL import Image
 
 
 def get_preprocessed_image(image_path: str, threshold: int = 128) -> Image.Image:
-    image = image.convert("L")
+    image = Image.open(image_path)
+    gray_image = image.convert("L")
 
     if threshold > 255 or threshold < 0:
-        return image.point(lambda x: 0 if x < 128 else 255, "1")
+        return gray_image.point(lambda x: 0 if x < 128 else 255, "1")
 
-    return image.point(lambda x: 0 if x < threshold else 255, "1")
+    return gray_image.point(lambda x: 0 if x < threshold else 255, "1")
