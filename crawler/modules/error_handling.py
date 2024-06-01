0b2f677908ebdb200fb9961aaa3c89c5e1f315e4
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-01 22:14:44 +0900

    paddle을 활용한 error handling 함수 추가.

--- crawler/modules/error_handling.py
+++ crawler/modules/error_handling.py
@@ -2,6 +2,7 @@ import os
 
 import pytesseract
 from PIL import Image
+from paddleocr import PaddleOCR
 
 from . import constants
 from .save_text_data import save_text_data
@@ -15,7 +16,8 @@ def save_all_texts_with_tesseract(dir_path: str):
         if not file_name.split(".")[-1] in constants.IMAGE_EXTENSIONS:
             continue
 
-        image = Image.open(f"{dir_path}/{file_name}")
+        image_path = os.path.join(dir_path, file_name)
+        image = Image.open(image_path)
         text = pytesseract.image_to_string(image, lang="kor")
 
         save_text_data(
@@ -23,4 +25,23 @@ def save_all_texts_with_tesseract(dir_path: str):
             file_name=f"all-text-data-{file_name}.txt",
             data=text,
             type="text",
+        )
+
+
+def save_all_texts_with_paddles(dir_path: str):
+    for file_name in os.listdir(dir_path):
+        if not file_name.split(".")[-1] in constants.IMAGE_EXTENSIONS:
+            continue
+
+        image_path = os.path.join(dir_path, file_name)
+
+        paddle_ocr = PaddleOCR(lang="korean", use_angle_cls=False)
+        result = paddle_ocr.ocr(image_path, cls=False)
+        ocr_result = result[0]
+
+        save_text_data(
+            save_dir=dir_path,
+            file_name=f"all-text-data-{file_name}.json",
+            data={"ocr_result": ocr_result},
+            type="json",
         )
