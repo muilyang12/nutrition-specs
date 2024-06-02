e67eb2099603925abc5d7515fc5f7741e29a1ed0
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-02 10:55:55 +0900

    openCV Morphological Transformations 기법 구현한 preprocess 함수 추가.

--- crawler/modules/image/get_preprocessed_image.py
+++ crawler/modules/image/get_preprocessed_image.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import cv2 as cv
+import numpy as np
 
 
 def get_preprocessed_image1(image_path: str, threshold: int = 128) -> Image.Image:
@@ -48,5 +49,33 @@ def get_preprocessed_image2(image_path: str) -> None:
 
     cv.imshow("Adaptive Thresholding - MEAN", adaptive_thresh_image1)
     cv.imshow("Adaptive Thresholding - GAUSSIAN", adaptive_thresh_image2)
+
+    cv.waitKey(0)
+
+
+# OpenCV Morphological Transformations
+# https://docs.opencv.org/4.6.0/d9/d61/tutorial_py_morphological_ops.html
+def get_preprocessed_image3(image_path: str) -> None:
+    image = cv.imread(image_path)
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    cv.imshow("Original - Grayscale", gray_image)
+
+    # Erosion
+    kernel = np.ones((3, 3), np.uint8)
+    erosion_image = cv.erode(gray_image, kernel, iterations=1)
+    cv.imshow("Erosion", erosion_image)
+
+    # Dilation
+    kernel = np.ones((3, 3), np.uint8)
+    dilation_image = cv.dilate(gray_image, kernel, iterations=1)
+    cv.imshow("Dilation", dilation_image)
+
+    # Opening
+    opening_image = cv.morphologyEx(gray_image, cv.MORPH_OPEN, kernel)
+    cv.imshow("Opening", opening_image)
+
+    # Closing
+    closing_image = cv.morphologyEx(gray_image, cv.MORPH_CLOSE, kernel)
+    cv.imshow("Closing", closing_image)
 
     cv.waitKey(0)
