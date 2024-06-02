7627693e7695d5f7264c52b476cedb39b697fdd7
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-02 21:09:58 +0900

    사진 파일 처리 중 에러 발생하는 경우에 대한 try except 처리 추가.

--- crawler/modules/save_nutrition_facts.py
+++ crawler/modules/save_nutrition_facts.py
@@ -13,34 +13,41 @@ def save_nutrition_facts(dir_path):
         if not file_name.split(".")[-1] in constants.IMAGE_EXTENSIONS:
             continue
 
-        image_path = os.path.join(dir_path, file_name)
-        search_targets = ["영양정보"]
-        text_positions = find_text_position_with_tesseract(image_path, search_targets)
+        try:
+            image_path = os.path.join(dir_path, file_name)
+            search_targets = ["영양정보"]
+            text_positions = find_text_position_with_tesseract(
+                image_path, search_targets
+            )
 
-        for target in search_targets:
-            if not text_positions[target]:
-                continue
+            for target in search_targets:
+                if not text_positions[target]:
+                    continue
 
-            save_count_per_dir += 1
+                save_count_per_dir += 1
 
-            positions = text_positions[target]
+                positions = text_positions[target]
 
-            for position in positions:
-                left, top, right, bottom = (
-                    position["left"],
-                    position["top"],
-                    position["right"],
-                    position["bottom"],
-                )
+                for position in positions:
+                    left, top, right, bottom = (
+                        position["left"],
+                        position["top"],
+                        position["right"],
+                        position["bottom"],
+                    )
 
-                crop_image(
-                    image_path=f"{dir_path}/{file_name}",
-                    save_as=f"nutrition-facts-{save_count_per_dir}.jpg",
-                    top=top - 50,
-                    bottom=bottom - 300,
-                    left=0,
-                    right=0,
-                )
+                    crop_image(
+                        image_path=f"{dir_path}/{file_name}",
+                        save_as=f"nutrition-facts-{save_count_per_dir}.jpg",
+                        top=top - 50,
+                        bottom=bottom - 300,
+                        left=0,
+                        right=0,
+                    )
+        except:
+            print(f"An error occurred while processing the file '{file_name}'.")
+
+            continue
 
     print(f"{save_count_per_dir} tables were found in the directory '{dir_path}'.")
 
