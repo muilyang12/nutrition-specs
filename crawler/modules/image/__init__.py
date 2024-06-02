3c66960d067a3aa09f1902f97a013b30f83ba2a4
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-02 10:56:21 +0900

    openCV Morphological Transformations 기법 구현한 preprocess 함수 추가. 2

--- crawler/modules/image/__init__.py
+++ crawler/modules/image/__init__.py
@@ -1,1 +1,6 @@
+from .get_preprocessed_image import (
+    get_preprocessed_image1,
+    get_preprocessed_image2,
+    get_preprocessed_image3,
+)
 from .compare_pillow_images import compare_pillow_images
