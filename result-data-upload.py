9b058343a955e0e85de68d543da548aa3a2e442c
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-08 19:12:12 +0900

    csv 파일의 row 맞춰 api 실행하는 스크립트 1차 완성.

--- result-data-upload.py
+++ result-data-upload.py
@@ -22,3 +22,54 @@ with open("result-data.csv", "r", encoding="utf-8") as f:
 
             enrolled_categories[category_key] = category_res["id"]
 
+        product_res = requests.post(
+            url="http://127.0.0.1:8000/food/product/",
+            data={
+                "food-category": enrolled_categories[category_key],
+                "brand_name": brand_name,
+                "product_name": product_name,
+                "coupang_url": coupang_url,
+            },
+        ).json()
+
+        product_id = product_res["id"]
+
+        (
+            serving_size,
+            serving_unit,
+            calories,
+            total_carbohydrate,
+            sugars,
+            sugar_alcohols,
+            dietary_fiber,
+            allulose,
+            total_fat,
+            saturated_fat,
+            trans_fat,
+            cholesterol,
+            protein,
+            sodium,
+            calcium,
+        ) = line[5:]
+
+        requests.post(
+            url="http://127.0.0.1:8000/food/nutrition/",
+            data={
+                "product": product_id,
+                "serving_size": serving_size,
+                "serving_unit": serving_unit,
+                "calories": calories,
+                "total_carbohydrate": total_carbohydrate,
+                "sugars": sugars,
+                "sugar_alcohols": sugar_alcohols,
+                "dietary_fiber": dietary_fiber,
+                "allulose": allulose,
+                "total_fat": total_fat,
+                "saturated_fat": saturated_fat,
+                "trans_fat": trans_fat,
+                "cholesterol": cholesterol,
+                "protein": protein,
+                "sodium": sodium,
+                "calcium": calcium,
+            },
+        )
