7afa09bfff2ffeb4f3e47ee611a86c862acfd1bd
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-09 12:49:50 +0900

    food category 모델 unique, index 조건 meta 클래스로 분리.

--- be/food/models.py
+++ be/food/models.py
@@ -3,8 +3,21 @@ from django.db.models import Index
 
 
 class FoodCategory(models.Model):
-    category_name = models.CharField(max_length=100, unique=True)
-    category_key = models.CharField(max_length=100, unique=True, db_index=True)
+    category_name = models.CharField(max_length=100)
+    category_key = models.CharField(max_length=100)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["category_key"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["category_name"], name="unique_category_name"
+            ),
+            models.UniqueConstraint(
+                fields=["category_key"], name="unique_category_key"
+            ),
+        ]
 
     def __str__(self):
         return f"{self.category_key} - {self.category_name}"
