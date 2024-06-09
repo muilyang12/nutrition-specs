970611f7b2f60382c14a8ee5232eca65c86632ca
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-09 12:44:23 +0900

    이미 등록된 food category에 대한 대응 추가.

--- result-data-upload.py
+++ result-data-upload.py
@@ -17,13 +17,18 @@ with open("result-data.csv", "r", encoding="utf-8") as f:
         category_key, category_name, brand_name, product_name, coupang_url = line[:5]
 
         if not category_key in enrolled_categories:
-            category_res = requests.post(
-                url="http://127.0.0.1:8000/food/food-category/",
-                data={
-                    "category_key": category_key,
-                    "category_name": category_name,
-                },
+            category_res = requests.get(
+                f"http://127.0.0.1:8000/food/food-category/category-key/{category_key}"
             ).json()
+
+            if not "id" in category_res:
+                category_res = requests.post(
+                    url="http://127.0.0.1:8000/food/food-category/",
+                    data={
+                        "category_key": category_key,
+                        "category_name": category_name,
+                    },
+                ).json()
 
             enrolled_categories[category_key] = category_res["id"]
 
