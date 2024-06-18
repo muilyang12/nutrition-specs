3832d9a895f02c10dafc86a364a9085f34b6f7e5
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-18 21:39:54 +0900

    crawler get products 동작 제외하고 일단 주석 처리.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,23 +1,23 @@
 from modules import (
     get_products,
-    get_product_details,
-    save_text_data,
-    save_coupang_content_images,
-    save_nutrition_facts,
+    # get_product_details,
+    # save_text_data,
+    # save_coupang_content_images,
+    # save_nutrition_facts,
 )
 
 products = get_products(search_tring="두유")
 
-for product in products:
-    product_name = product["name"]
+# for product in products:
+#     product_name = product["name"]
 
-    details = get_product_details(product=product)
-    save_text_data(
-        save_dir=f"./data/{product_name}/",
-        file_name="details.json",
-        data={"details": details},
-        type="json",
-    )
-    save_coupang_content_images(save_dir=f"./data/{product_name}/", details=details)
+#     details = get_product_details(product=product)
+#     save_text_data(
+#         save_dir=f"./data/{product_name}/",
+#         file_name="details.json",
+#         data={"details": details},
+#         type="json",
+#     )
+#     save_coupang_content_images(save_dir=f"./data/{product_name}/", details=details)
 
-    save_nutrition_facts(dir_path=f"./data/{product_name}/")
+#     save_nutrition_facts(dir_path=f"./data/{product_name}/")
