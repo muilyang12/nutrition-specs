2111bcebc29285489b59f34d56c652591186a472
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-19 10:02:14 +0900

    잘못 커밋된 주석 삭제.

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -25,26 +25,3 @@ window.mainloop()
 #     save_coupang_content_images(save_dir=f"./data/{product_name}/", details=details)
 
 #     save_nutrition_facts(dir_path=f"./data/{product_name}/")
-
-
-# from PIL import ImageGrab
-
-
-# def show_input():
-#     entered_text = entry.get()
-#     print(entered_text)
-#     entry.delete(0, "end")
-
-
-# def screenshot_mode(event):
-#     window.iconify()  # Minimize window
-#     x0, y0, x1, y1 = 100, 100, 500, 500
-#     screenshot = ImageGrab.grab(bbox=(x0, y0, x1, y1))
-
-
-# def close_window(event):
-#     window.destroy()
-
-
-# window.bind("<Control-q>", close_window)
-# window.bind("<Control-s>", screenshot_mode)
