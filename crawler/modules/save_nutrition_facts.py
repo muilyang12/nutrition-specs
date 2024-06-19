2496517f944cfe58634fbbbdb4f90737f1c1bcf6
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-19 10:20:37 +0900

    tesseract 말고 paddle 사용해서 nutrition facts 사진 탐색하도록 변경.

--- crawler/modules/save_nutrition_facts.py
+++ crawler/modules/save_nutrition_facts.py
@@ -1,12 +1,14 @@
 import os
 
 from . import constants
-from .text_position import find_text_position_with_tesseract
+from .text_position import find_text_position_with_paddle
 from .image import crop_image
 from .error_handling import save_all_texts_with_tesseract
 
 
 def save_nutrition_facts(dir_path):
+    print(f"Started to search in {dir_path}")
+
     save_count_per_dir = 0
 
     for file_name in os.listdir(dir_path):
@@ -16,9 +18,8 @@ def save_nutrition_facts(dir_path):
         try:
             image_path = os.path.join(dir_path, file_name)
             search_targets = ["영양정보"]
-            text_positions = find_text_position_with_tesseract(
-                image_path, search_targets
-            )
+            # text_positions = find_text_position_with_tesseract(image_path, search_targets)
+            text_positions = find_text_position_with_paddle(image_path, search_targets)
 
             for target in search_targets:
                 if not text_positions[target]:
