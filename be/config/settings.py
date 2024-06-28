9ff6d39574d85576f1cd4dd2beff5ff06a80c6fe
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-28 09:01:11 +0900

    pagenation 설정 추가.

--- be/config/settings.py
+++ be/config/settings.py
@@ -50,6 +50,8 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
         "rest_framework.renderers.BrowsableAPIRenderer",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 MIDDLEWARE = [
--- be/food/views.py
+++ be/food/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
+from rest_framework.pagination import PageNumberPagination
 from . import models, serializers
 
 
@@ -31,6 +32,7 @@ class BrandViewSet(viewsets.ModelViewSet):
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductSerializer
+    pagination_class = PageNumberPagination
 
     def list(self, request, *args, **kwargs):
         category_id = request.query_params.get("food-category")
@@ -39,21 +41,21 @@ class ProductViewSet(viewsets.ModelViewSet):
         if not category_id and not category_key:
             raise ValidationError(code=status.HTTP_400_BAD_REQUEST)
 
-        data = None
-
         if category_id:
             result = self.queryset.filter(food_categories=category_id).distinct()
-            serializer = self.get_serializer(result, many=True)
-            data = serializer.data
 
         elif category_key:
             result = self.queryset.filter(
                 food_categories__category_key=category_key
             ).distinct()
-            serializer = self.get_serializer(result, many=True)
-            data = serializer.data
 
-        return Response(data)
+        result_with_page = self.paginate_queryset(result)
+        if result_with_page:
+            serializer = self.get_serializer(result_with_page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(result, many=True)
+        return Response(serializer.data)
 
 
 class NutritionViewSet(viewsets.ModelViewSet):
