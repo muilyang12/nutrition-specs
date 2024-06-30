dbbbcb96bcb86d1478258a60f2bb05e3b9a4bc1b
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-06-30 23:00:50 +0900

    filter 값 변경에 따라 api가 호출되도록 추가.

--- fe/src/components/product-list/ProductList.client.tsx
+++ fe/src/components/product-list/ProductList.client.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useBrandFilterStore } from "@stores/brandFilterStore";
 import ProductCard from "@components/product-card/ProductCard";
+import { foodApi } from "@apis/food";
 import { ProductsAndNutritions } from "./ProductList.define";
 import styles from "./ProductList.module.css";
 
@@ -15,6 +17,13 @@ export default function ProductList(props: Props) {
 
   const [productsAndNutritions, setProductsAndNutritions] = useState(initialProductsAndNutritions);
 
+  const { selectedFilters } = useBrandFilterStore();
+
+  useEffect(() => {
+    foodApi.getProducts(selectedFoodCategoryKey, selectedFilters).then((data) => {
+      console.log(data.results);
+    });
+  }, [selectedFilters]);
   return (
     <div className={styles.productListWrapper}>
       {productsAndNutritions.map(([product, nutrition]) => (
