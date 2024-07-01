761b6142b56882b44b8e65393b58fed75beabe04
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-01 18:23:06 +0900

    crawler ui 대폭 변경.

--- crawler/modules/CrawlerUI.py
+++ crawler/modules/CrawlerUI.py
@@ -9,33 +9,56 @@ class CrawlerUI:
         self.create_widgets()
 
     def create_widgets(self):
-        self.query_label = tk.Label(self.window, text="Search Query")
-        self.query_label.grid(row=0, column=0, padx=10, pady=10)
-        self.query_entry = tk.Entry(self.window)
-        self.query_entry.grid(row=0, column=1, padx=10, pady=10)
-
         self.name_label = tk.Label(self.window, text="Category Name")
-        self.name_label.grid(row=1, column=0, padx=10, pady=10)
+        self.name_label.grid(row=0, column=0, padx=10, pady=10)
         self.name_entry = tk.Entry(self.window)
-        self.name_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.name_entry.grid(row=0, column=1, padx=10, pady=10)
 
         self.key_label = tk.Label(self.window, text="Category Key")
-        self.key_label.grid(row=2, column=0, padx=10, pady=10)
+        self.key_label.grid(row=0, column=2, padx=10, pady=10)
         self.key_entry = tk.Entry(self.window)
-        self.key_entry.grid(row=2, column=1, padx=10, pady=10)
+        self.key_entry.grid(row=0, column=3, padx=10, pady=10)
+
+        self.category_button = tk.Button(self.window, text="Add Category")
+        self.category_button.grid(row=0, column=4, padx=10, pady=10)
+
+        self.category_listbox = tk.Listbox(self.window, selectmode=tk.MULTIPLE)
+        self.category_listbox.grid(row=0, column=5, padx=1, pady=10)
+
+        # =========================
+
+        self.brand_name_label = tk.Label(self.window, text="Brand Name")
+        self.brand_name_label.grid(row=1, column=0, padx=10, pady=10)
+        self.brand_name_entry = tk.Entry(self.window)
+        self.brand_name_entry.grid(row=1, column=1, padx=10, pady=10)
+
+        self.brand_button = tk.Button(self.window, text="Add Brand")
+        self.brand_button.grid(row=1, column=4, padx=10, pady=10)
+
+        self.brand_combobox = ttk.Combobox(self.window, values=["QQQQQ", "WWWWW"])
+        self.brand_combobox.grid(row=1, column=5, columnspan=2, padx=10, pady=10)
+
+        # =========================
+
+        self.query_label = tk.Label(self.window, text="Search Query")
+        self.query_label.grid(row=2, column=0, padx=10, pady=10)
+        self.query_entry = tk.Entry(self.window)
+        self.query_entry.grid(row=2, column=1, padx=10, pady=10)
 
         self.search_button = tk.Button(self.window, text="Search")
-        self.search_button.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+        self.search_button.grid(row=2, column=4, columnspan=2, padx=10, pady=10)
+
+        # =========================
 
         self.manual_collect_button = tk.Button(self.window, text="Manual Collecting")
-        self.manual_collect_button.grid(row=4, column=0, columnspan=1, padx=10, pady=10)
+        self.manual_collect_button.grid(row=3, column=0, padx=10, pady=10)
 
         self.semi_auto_collect_button = tk.Button(
             self.window, text="Semi-Auto Collecting"
         )
-        self.semi_auto_collect_button.grid(
-            row=4, column=1, columnspan=1, padx=10, pady=10
-        )
+        self.semi_auto_collect_button.grid(row=3, column=1, padx=10, pady=10)
+
+        # =========================
 
         self.columns = (
             "category_name",
@@ -57,4 +80,4 @@ class CrawlerUI:
             else:
                 self.tree.column(col, width=100, stretch=tk.YES)
 
-        self.tree.grid(row=5, column=0, columnspan=3, padx=10, pady=10, sticky="nsew")
+        self.tree.grid(row=4, column=0, columnspan=7, padx=10, pady=10, sticky="nsew")
