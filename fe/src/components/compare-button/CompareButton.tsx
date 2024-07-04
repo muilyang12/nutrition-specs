d7392088a160861ef790d5de22c6de1f8883a351
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-05 08:45:08 +0900

    comparing -> compare path 변경

--- fe/src/components/compare-button/CompareButton.tsx
+++ fe/src/components/compare-button/CompareButton.tsx
@@ -18,7 +18,7 @@ export default function CompareButton() {
       { arrayFormat: "repeat" }
     );
 
-    router.push(`/comparing?${queries}`);
+    router.push(`/compare?${queries}`);
   };
 
   return (
