2f39591d9eecd1e7c3622b8473e22e3c0b84794b
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-04 21:12:32 +0900

    compare store에 selected products 상태 추가.

--- fe/src/stores/compareStore.ts
+++ fe/src/stores/compareStore.ts
@@ -3,9 +3,19 @@ import { create } from "zustand";
 interface CompareStore {
   isComparing: boolean;
   toggleIsComparing: () => void;
+  selectedProducts: number[];
+  addSelectedProduct: (newProductId: number) => void;
+  removeSelectedProduct: (newProductId: number) => void;
 }
 
 export const useCompareStore = create<CompareStore>()((set) => ({
   isComparing: false,
   toggleIsComparing: () => set((state) => ({ isComparing: !state.isComparing })),
+  selectedProducts: [],
+  addSelectedProduct: (newProductId: number) =>
+    set((state) => ({ selectedProducts: [...state.selectedProducts, newProductId] })),
+  removeSelectedProduct: (newProductId: number) =>
+    set((state) => ({
+      selectedProducts: state.selectedProducts.filter((id) => id !== newProductId),
+    })),
 }));
