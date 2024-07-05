d73a27efcd6cfc966b98dac2bdd61d4541fe162a
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-05 10:58:18 +0900

    comparing data 1차 완성.

--- fe/src/components/comparing-data/ComparingData.tsx
+++ fe/src/components/comparing-data/ComparingData.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import ComparingDataMainTable from "./ComparingDataMainTable";
+import styles from "./ComparingData.module.css";
+
 export default function ComparingData() {
-  return <></>;
+  return (
+    <>
+      <div className={styles.comparingDataWrapper}>
+        <span className={styles.comparingDataTitle}>Nutrition</span>
+
+        <div className={styles.comparingDataParts}>
+          <div className={styles.comparingDataPart}>
+            <span className={styles.comparingDataPartTitle}>주요 영양소</span>
+            <ComparingDataMainTable />
+          </div>
+
+          <div className={styles.comparingDataPart}>
+            <span className={styles.comparingDataPartTitle}>상세</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
