c0ca39e3bd97b7ca34497bf17082252a53d33ba3
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-05 10:54:59 +0900

    comparing data main table 컴포넌트 추가.

---
+++ fe/src/components/comparing-data/ComparingDataMainTable.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { NUTRITION_KEY_NAME_MAPPER } from "@defines/nutrition";
+import styles from "./ComparingDataMainTable.module.css";
+import { useComparingProductsStore } from "@stores/comparingProductsStore";
+
+export default function ComparingDataMainTable() {
+  const { comparingProducts } = useComparingProductsStore();
+
+  return (
+    <table className={styles.table}>
+      <thead>
+        <tr>
+          <td className={styles.tableIndexCol}></td>
+          <td>{NUTRITION_KEY_NAME_MAPPER["calories"]}</td>
+          <td>{NUTRITION_KEY_NAME_MAPPER["total_carbohydrate"]}</td>
+          <td>{NUTRITION_KEY_NAME_MAPPER["sugars"]}</td>
+          <td>{NUTRITION_KEY_NAME_MAPPER["total_fat"]}</td>
+          <td>{NUTRITION_KEY_NAME_MAPPER["protein"]}</td>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.values(comparingProducts).map((product, index) => {
+          const nutrition = product.nutritions[0];
+
+          return (
+            <tr>
+              <td className={styles.tableIndexCol}>{index + 1}</td>
+              <td>{`${nutrition.data.calories} kcal`}</td>
+              <td>{`${nutrition.data.total_carbohydrate}g`}</td>
+              <td>{`${nutrition.data.sugars}g`}</td>
+              <td>{`${nutrition.data.total_fat}g`}</td>
+              <td>{`${nutrition.data.protein}g`}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
