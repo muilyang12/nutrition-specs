4a04d002f02a540adf00fe6f2cd476dc02faf636
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-08 18:10:46 +0900

    ecr 이름 변경한 것 be upload 스크립트에 반영.

--- deploy-be-ecr-upload.sh
+++ deploy-be-ecr-upload.sh
@@ -10,13 +10,13 @@ echo -e "\n\nRemoving all Docker images..."
 docker rmi -f $(docker images -q)
 
 echo -e "\n\nBuilding Docker image..."
-docker build -t nutrition-comparison-be ./be
+docker build -t nutriinsights-be ./be
 
 echo -e "\n\nTagging Docker image..."
-docker tag nutrition-comparison-be:latest 590183905213.dkr.ecr.ap-northeast-2.amazonaws.com/nutrition-comparison-be:latest
+docker tag nutriinsights-be:latest 590183905213.dkr.ecr.ap-northeast-2.amazonaws.com/nutriinsights-be:latest
 
 echo -e "\n\nLogging in to AWS ECR..."
 /mnt/c/Program\ Files/Amazon/AWSCLIV2/aws.exe ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 590183905213.dkr.ecr.ap-northeast-2.amazonaws.com
 
 echo -e "\n\nPushing Docker image to ECR..."
-docker push 590183905213.dkr.ecr.ap-northeast-2.amazonaws.com/nutrition-comparison-be:latest
+docker push 590183905213.dkr.ecr.ap-northeast-2.amazonaws.com/nutriinsights-be:latest
