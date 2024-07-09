ef502fb17fd60aa88475a265f0dacdbbadc1895a
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-09 21:26:35 +0900

    serializers 파일 분리. 2

---
+++ be/food/serializers/__init__.py
@@ -0,0 +1,5 @@
+from .food_category_serializer import FoodCategorySerializer, FoodCategoryGetSerializer
+from .brand_serializer import BrandSerializer
+from .product_serializer import ProductSerializer, ProductGetSerializer
+from .nutrition_serializer import NutritionSerializer
+from .product_nutrition_serializer import ProductNutritionSerializer
