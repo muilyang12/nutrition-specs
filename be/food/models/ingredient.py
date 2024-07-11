eb2e0b7385e193f7d2c88efa51ecc5fb7e4fee75
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-11 11:07:31 +0900

    ingredient 모델 index, constraint 조건 추가.

--- be/food/models/ingredient.py
+++ be/food/models/ingredient.py
@@ -5,5 +5,16 @@ class Ingredient(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
 
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name"],
+                name="unique_name",
+            ),
+        ]
+
     def __str__(self):
         return self.name
