e4fa8418694cd0311299a51814426d94cc073d5d
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-11 14:18:25 +0900

    product detail api에 ingredient 정보가 같이 오도록 변경.

--- be/food/serializers/product_datail_serializer.py
+++ be/food/serializers/product_datail_serializer.py
@@ -1,16 +1,24 @@
 from rest_framework import serializers
 
 from .. import models
-from . import NutritionSerializer
+from . import NutritionSerializer, ProductIngredientSerializer
 
 
 class ProductDatailSerializer(serializers.ModelSerializer):
     brand_name = serializers.SerializerMethodField()
     nutritions = NutritionSerializer(many=True, read_only=True)
+    ingredients = ProductIngredientSerializer(many=True, read_only=True)
 
     class Meta:
         model = models.Product
-        fields = ["id", "brand_name", "product_name", "coupang_url", "nutritions"]
+        fields = [
+            "id",
+            "brand_name",
+            "product_name",
+            "coupang_url",
+            "nutritions",
+            "ingredients",
+        ]
 
     def get_brand_name(self, obj):
         return obj.brand.name if obj.brand else None
