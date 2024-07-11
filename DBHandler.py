780d333de0ea166240c8c13698504b94fb91ac56
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-11 14:39:15 +0900

    db handler 클래스 추가.

---
+++ DBHandler.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+
+class DBHandler:
+    def __init__(self, db_path) -> None:
+        self.db_path = db_path
+
+        self.connection = None
+        self.cursor = None
+
+    def connect(self):
+        self.connection = sqlite3.connect(self.db_path)
+        self.cursor = self.connection.cursor()
+
+    def disconnect(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
+
+    def get_table_names(self):
+        if not self.cursor:
+            raise Exception("Database not connected. Call connect() first.")
+
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+
+        tables = self.cursor.fetchall()
+
+        return [table[0] for table in tables]
+
+    def get_table_data(self, table_name):
+        if not self.cursor:
+            raise Exception("Database not connected. Call connect() first.")
+
+        self.cursor.execute(f"SELECT * FROM {table_name}")
+
+        rows = self.cursor.fetchall()
+
+        if rows:
+            for row in rows:
+                print(row)
+        else:
+            print(f"No data found in table '{table_name}'")
+
