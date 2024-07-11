209cd078d0bb7de16648ec4319def338f081139a
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-11 11:26:14 +0900

    product ingredient 모델 추가.

---
+++ be/food/models/product_ingredient.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from . import Product
+from . import Ingredient
+
+
+class ProductIngredient(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.PROTECT, related_name="ingredients", null=True
+    )
+    s3_key = models.CharField(max_length=300, null=True, blank=True)
+    ingredients = models.ManyToManyField(Ingredient)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["product"]),
+        ]
+
+    def __str__(self):
+        brand_name = (
+            self.product.brand.name if self.product and self.product.brand else "None"
+        )
+        product_name = self.product.product_name if self.product else "None"
+
+        return f"{brand_name} - {product_name}"
