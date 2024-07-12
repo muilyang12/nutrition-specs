72227d2c5a2c4dbe9ba26e32ca763fb07a93c571
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-12 19:49:47 +0900

    도메인 nutrition-specs.com으로 변경.

--- fe/src/apis/axiosInstance.ts
+++ fe/src/apis/axiosInstance.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 const isProd = process.env.NODE_ENV == "production";
 
 export const axiosInstance = axios.create({
-  baseURL: isProd ? "https://api.whatgodsays.net/" : "http://127.0.0.1:8000/",
+  baseURL: isProd ? "https://api.nutrition-specs.com/" : "http://127.0.0.1:8000/",
 });
