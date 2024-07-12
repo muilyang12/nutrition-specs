1a894e587a3a7e8337abf0edf3fa294cbb430691
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-12 11:26:33 +0900

    tree view 추가 및 db 값 불러와서 tree 채우는 동작까지 수행.

--- crawler/modules/db_handler/DBHandlerUI.py
+++ crawler/modules/db_handler/DBHandlerUI.py
@@ -1,15 +1,44 @@
 import tkinter as tk
 from tkinter import ttk
 
+from .DBHandle import DBHandle
+
 
 class DBHandlerUI:
     def __init__(self, app):
         self.app = app
 
+        self.dbHandle = DBHandle("../be/db.sqlite3")
+        self.dbHandle.connect()
+
+        self.inputs = []
+
         self.create_widgets()
+        self.load_values()
 
     def create_widgets(self):
-        self.name_label = tk.Label(self.app.window, text="Category Name")
-        self.name_label.grid(row=0, column=0, padx=10, pady=10)
-        self.name_entry = tk.Entry(self.app.window)
-        self.name_entry.grid(row=0, column=1, padx=10, pady=10)
+        columns = self.dbHandle.get_column_names("food_product")
+
+        self.columns = tuple(columns)
+        self.column_index = {col: idx for idx, col in enumerate(self.columns)}
+        self.tree = ttk.Treeview(self.app.window, columns=self.columns, show="headings")
+
+        for col in self.columns:
+            self.tree.heading(col, text=col.replace("_", " ").title())
+
+            if col == "product_name":
+                self.tree.column(col, width=200, stretch=tk.YES)
+            else:
+                self.tree.column(col, width=100, stretch=tk.YES)
+
+        self.tree.grid(row=0, column=0, columnspan=7, padx=10, pady=10, sticky="nsew")
+
+    def load_values(self):
+        table_data = self.dbHandle.get_table_data("food_product")
+
+        for data in table_data:
+            self.tree.insert(
+                "",
+                tk.END,
+                values=data,
+            )
