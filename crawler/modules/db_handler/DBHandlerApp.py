79fbc2cfb4bcb32c6f5524a7eac22ef0643bab71
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-12 11:51:19 +0900

    screenshot tool 연결.

--- crawler/modules/db_handler/DBHandlerApp.py
+++ crawler/modules/db_handler/DBHandlerApp.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 
 from .DBHandlerUI import DBHandlerUI
+from ..CrawlerScreenshotTool import CrawlerScreenshotTool
 
 
 class DBHandlerApp:
     def __init__(self):
+        self.current_image = None
+
         self.window = tk.Tk()
         self.window.title("DB Handler")
         self.window.geometry("1000x600")
+
+        self.screenshot_tool = CrawlerScreenshotTool(app=self)
 
         self.ui = DBHandlerUI(app=self)
 
