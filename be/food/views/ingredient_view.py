f0a1a9dcc1d48cf329e85a9b1d6580f493177169
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-07-18 11:25:03 +0900

    ingredient list에 query params로 id를 넘겨야 하도록 api 변경.

--- be/food/views/ingredient_view.py
+++ be/food/views/ingredient_view.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 from .. import models, serializers
 
@@ -6,3 +8,14 @@ from .. import models, serializers
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = models.Ingredient.objects.all()
     serializer_class = serializers.IngredientSerializer
+
+    def list(self, request, *args, **kwargs):
+        ingredient_ids = request.query_params.getlist("ingredient")
+
+        if not ingredient_ids:
+            return ValidationError(code=status.HTTP_400_BAD_REQUEST)
+
+        result = self.queryset.filter(id__in=ingredient_ids)
+        serializer = self.get_serializer(result, many=True)
+
+        return Response(serializer.data)
