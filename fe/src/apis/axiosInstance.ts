fcea0012a1c7ff00e126660e0dd324d22a02a938
DESKTOP-QAHFTIT\Yang <muilyang12@gmail.com>
2024-11-26 21:09:23 -0600

    Set to use localhost during the build process.

--- fe/src/apis/axiosInstance.ts
+++ fe/src/apis/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const isProd = process.env.NODE_ENV == "production";
+const isBuilding = process.env.IS_BUILDING === "true";
+const isProd = !isBuilding && process.env.NODE_ENV == "production";
 
 export const axiosInstance = axios.create({
   baseURL: isProd ? "https://api.nutrition-specs.com/" : "http://127.0.0.1:8000/",
